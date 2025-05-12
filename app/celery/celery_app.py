477124ac4dfdb679953d3412351c1b25143de21f
Igor Kralichev <ikralichev@list.ru>
2025-05-12 16:44:14 +0300

    initial commit

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -5,7 +5,8 @@ from app.config import Config
 celery = Celery(
     __name__,
     broker=Config.CELERY_BROKER_URL,
-    backend=Config.CELERY_RESULT_BACKEND
+    backend=Config.CELERY_RESULT_BACKEND,
+    include=['app.celery.tasks'],
 )
 
 celery.conf.update(
